fd9db1bf363c78f953fcf8bc400480245b9b2957
Islam Kasem <is_kasem@yahoo.com>
2018-12-15 19:45:49 +0300

    some changes done for adding print statement

--- testCloningProjectCourse3/ViewController.swift
+++ testCloningProjectCourse3/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+    print ("some code changes ")
     }
 
     override func didReceiveMemoryWarning() {
